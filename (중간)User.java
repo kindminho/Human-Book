1f26e9ed6087d0e32869b56edda7ed2924f2d50b
mino_KIM <66734027+kindminho@users.noreply.github.com>
2020-07-15 23:19:58 +0900

    Create (중간)User.java

---
+++ (중간)User.java
@@ -0,0 +1,14 @@
+package team.everywhere.humanbook;
+
+public class User {
+    String email;
+
+    public String getEmail() {    // 마우스 우클릭 → Generate → Getter and Setter
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
